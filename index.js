c36e93297c75e967837842158cb4d1273939c94c
HuangjieJin <jinhuangjie233@gmail.com>
2021-03-24 12:28:49 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(collection, name){
+    return collection.filter(function(n){
+      return n===name||n===name[0].toLowerCase()+name.slice(1);
+    })
+  };
+
+function fuzzyMatch(collection, name){
+    return collection.filter(function(n){
+        return n[0]===name[0];
+    })
+};
+
+function matchName(collection, name){
+    return collection.filter(function(n){
+        return n.name===name;
+    })
+}
